fb4161f33e323ad746fcd2a85df9755d19fd5ba7
jpguizelini <ja1dev@proton.me>
2026-01-25 21:44:19 -0300

    chore: enable cors for frontend dev

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,12 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
   }));
 
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
