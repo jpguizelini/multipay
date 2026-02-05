98f83eb6eaa2be7f2b6859223877b54b6793b037
jpguizelini <ja1dev@proton.me>
2026-02-05 11:48:26 -0300

    fix: enable CORS for frontend

--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,8 @@ async function bootstrap() {
   }));
 
   app.enableCors({
-    origin: 'http://localhost:3001',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Accept',
+    origin: true,
+    credentials: true,
   });
 
   await app.listen(process.env.PORT ?? 3000);
